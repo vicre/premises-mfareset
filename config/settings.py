4af25ea4ea1dea35cfe500db004992707896b3be
Victor Reipur <vicre@dtu.dk>
2026-04-21 15:08:29 +0200

    fix: add allowed host

--- config/settings.py
+++ config/settings.py
@@ -47,14 +47,15 @@ DEBUG = os.getenv("DJANGO_DEBUG", "false").lower() == "true"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
+    "view.security.ait.dtu.dk"
     ]
 
 # Needs to be validated if this is needed, or has weaknesses, before being put in production
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://mfa-dtu.vezit.net",
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    "https://view.security.ait.dtu.dk",
+]
 
 
 ##### CAS (dtubase login portal) #####
